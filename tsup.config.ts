218d17654bbf446710396cfe6a6a0191f65e9cfb
bigmistqke <vandijckv@gmail.com>
2024-06-03 20:02:37 +0200

    remove tests from github actions

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,20 +2,13 @@ import { defineConfig } from 'tsup'
 import * as preset from 'tsup-preset-solid'
 
 const preset_options: preset.PresetOptions = {
-  // array or single object
   entries: [
-    // default entry (index)
     {
-      // entries with '.tsx' extension will have `solid` export condition generated
       entry: 'src/index.tsx',
-      // will generate a separate development entry
       dev_entry: true,
     },
   ],
-  // Set to `true` to remove all `console.*` calls and `debugger` statements in prod builds
   drop_console: true,
-  // Set to `true` to generate a CommonJS build alongside ESM
-  // cjs: true,
 }
 
 const CI =
