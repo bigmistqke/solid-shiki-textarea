ece2a38e6af859156d8162f6dabf65370eed8259
bigmistqke <vandijckv@gmail.com>
2024-06-04 00:56:42 +0200

    tweak dev

--- dev/index.tsx
+++ dev/index.tsx
@@ -1,4 +1,4 @@
-import { For, createSignal, useTransition, type Component } from 'solid-js'
+import { For, createSignal, type Component } from 'solid-js'
 import { render } from 'solid-js/web'
 import { ShikiTextarea } from '../src'
 import './index.css'
@@ -7,27 +7,32 @@ import { Lang, Theme, langs, themes } from './shiki-data'
 const App: Component = () => {
   const [currentTheme, setCurrentTheme] = createSignal<Theme>(themes[0])
   const [currentLang, setCurrentLang] = createSignal<Lang>('tsx')
-  const startTransition = useTransition()[1]
 
   return (
     <div class="App">
       <header>
         <h1>Solid Shiki Textarea</h1>
         <div class="inputs">
-          <label>themes</label>
-          <select
-            value={currentTheme()}
-            onInput={e => setCurrentTheme(e.currentTarget.value as Theme)}
-          >
-            <For each={themes}>{theme => <option>{theme}</option>}</For>
-          </select>
-          <label>languages</label>
-          <select
-            value={currentLang()}
-            onInput={e => setCurrentLang(e.currentTarget.value as Lang)}
-          >
-            <For each={langs}>{language => <option>{language}</option>}</For>
-          </select>
+          <div>
+            <label for="theme">themes</label>
+            <select
+              id="theme"
+              value={currentTheme()}
+              onInput={e => setCurrentTheme(e.currentTarget.value as Theme)}
+            >
+              <For each={themes}>{theme => <option>{theme}</option>}</For>
+            </select>
+          </div>
+          <div>
+            <label for="lang">languages</label>
+            <select
+              id="lang"
+              value={currentLang()}
+              onInput={e => setCurrentLang(e.currentTarget.value as Lang)}
+            >
+              <For each={langs}>{language => <option>{language}</option>}</For>
+            </select>
+          </div>
         </div>
       </header>
       <main>
